8fc7af94e338796695bd00c6cdb4648246678b69
Philipp Baumann <phipsi@gmx.ch>
2024-04-10 17:09:50 +0200

    first, and running!

--- app/app.py
+++ app/app.py
@@ -1,53 +1,53 @@
 import os
 from flask import Flask, request, jsonify
-from flask_pymongo import PyMongo
+# from flask_pymongo import PyMongo
 
 application = Flask(__name__)
 
-application.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
+# application.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
 
-mongo = PyMongo(application)
-db = mongo.db
+# mongo = PyMongo(application)
+# db = mongo.db
 
 @application.route('/')
 def index():
     return jsonify(
         status=True,
-        message='Welcome to the Dockerized Flask MongoDB app!'
+        message='Welcome to the Dockerized Flask app!'
     )
 
-@application.route('/todo')
-def todo():
-    _todos = db.todo.find()
+# @application.route('/todo')
+# def todo():
+#     _todos = db.todo.find()
 
-    item = {}
-    data = []
-    for todo in _todos:
-        item = {
-            'id': str(todo['_id']),
-            'todo': todo['todo']
-        }
-        data.append(item)
+#     item = {}
+#     data = []
+#     for todo in _todos:
+#         item = {
+#             'id': str(todo['_id']),
+#             'todo': todo['todo']
+#         }
+#         data.append(item)
 
-    return jsonify(
-        status=True,
-        data=data
-    )
+#     return jsonify(
+#         status=True,
+#         data=data
+#     )
 
-@application.route('/todo', methods=['POST'])
-def createTodo():
-    data = request.get_json(force=True)
-    item = {
-        'todo': data['todo']
-    }
-    db.todo.insert_one(item)
+# @application.route('/todo', methods=['POST'])
+# def createTodo():
+#     data = request.get_json(force=True)
+#     item = {
+#         'todo': data['todo']
+#     }
+#     db.todo.insert_one(item)
 
-    return jsonify(
-        status=True,
-        message='Todo saved successfully!'
-    ), 201
+#     return jsonify(
+#         status=True,
+#         message='Todo saved successfully!'
+#     ), 201
 
 if __name__ == "__main__":
     ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
-    ENVIRONMENT_PORT = os.environ.get("APP_PORT", 5000)
+    ENVIRONMENT_PORT = os.environ.get("APP_PORT", 5020)
     application.run(host='0.0.0.0', port=ENVIRONMENT_PORT, debug=ENVIRONMENT_DEBUG)

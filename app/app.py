80aa4f143a817479109d92eba03fb1123b990a87
Philipp Baumann <phipsi@gmx.ch>
2024-05-16 09:03:50 +0200

    with second service

--- app/app.py
+++ app/app.py
@@ -1,23 +1,21 @@
-import os
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, render_template
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 
-application = Flask(__name__)
-# application.config['APPLICATION_ROOT'] = '/dtcui'
-application.wsgi_app = ProxyFix(
-    application.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
 )
 
-@application.route('/')
+@app.route('/')
 def index():
     # return jsonify(
     #     status=True,
-    #     message='Welcome to the Dockerized Flask application!'
+    #     message='Welcome to the Dockerized Flask app!'
     # )
     return render_template('index.html')
 
-# @application.route('/todo')
+# @app.route('/todo')
 # def todo():
 #     _todos = db.todo.find()
 
@@ -28,14 +26,14 @@ def index():
 #             'id': str(todo['_id']),
 #             'todo': todo['todo']
 #         }
-#         data.applicationend(item)
+#         data.append(item)
 
 #     return jsonify(
 #         status=True,
 #         data=data
 #     )
 
-# @application.route('/todo', methods=['POST'])
+# @app.route('/todo', methods=['POST'])
 # def createTodo():
 #     data = request.get_json(force=True)
 #     item = {
@@ -49,6 +47,4 @@ def index():
 #     ), 201
 
 if __name__ == "__main__":
-    ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
-    ENVIRONMENT_PORT = os.environ.get("APP_PORT", 5020)
-    application.run(host='0.0.0.0', port=ENVIRONMENT_PORT, debug=ENVIRONMENT_DEBUG)
+     app.run()
